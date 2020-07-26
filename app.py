70ca36b3ef54063ffc9ca7f86491b2c9a501c3a7
Rishabh Bhardwaj <rishabh.bh22@gmail.com>
2020-07-26 19:40:04 +0530

    chat room and save chat functionality implemented.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
+from datetime import datetime
+from bson.json_util import dumps
 from flask import Flask, render_template, redirect, request, url_for
 from flask_login import LoginManager, current_user, login_user, login_required, logout_user
 from flask_socketio import SocketIO, join_room, leave_room
 from pymongo.errors import DuplicateKeyError
-from db import get_user, save_user
-import os
-
+from db import get_user, save_user, get_rooms_for_user, save_room, add_room_members, get_room, is_room_member, \
+    get_room_members, get_messages, is_room_admin, update_room, remove_room_members, save_message
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -16,7 +18,10 @@ login_manager.init_app(app)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    rooms = []
+    if current_user.is_authenticated:
+        rooms = get_rooms_for_user(current_user.username)
+    return render_template("index.html", rooms=rooms)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -63,16 +68,75 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route('/chat')
+@app.route('/create-room/', methods=['GET', 'POST'])
 @login_required
-def chat():
-    username = request.args.get('username')
-    room = request.args.get('room')
+def create_room():
+    message = ''
+    if request.method == 'POST':
+        room_name = request.form.get('room_name')
+        usernames = [username.strip() for username in request.form.get('members').split(',')]
 
-    if username and room:
-        return render_template('chat.html', username=username, room=room)
+        if len(room_name) and len(usernames):
+            room_id = save_room(room_name, current_user.username)
+            if current_user.username in usernames:
+                usernames.remove(current_user.username)
+            add_room_members(room_id, room_name, usernames, current_user.username)
+            return redirect(url_for('view_room', room_id=room_id))
+        else:
+            message = 'Failed to create room! Please try again later.'
+    return render_template('create_room.html', message=message)
+
+
+@app.route('/rooms/<room_id>/')
+@login_required
+def view_room(room_id):
+    room = get_room(room_id)
+    if room and is_room_member(room_id, current_user.username):
+        room_members = get_room_members(room_id)
+        messages = get_messages(room_id)
+        return render_template('view_room.html', username=current_user.username, room=room, room_members=room_members,
+                               messages=messages)
     else:
-        return redirect(url_for('home'))
+        return "Room not found", 404
+
+
+@app.route('/rooms/<room_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_room(room_id):
+    room = get_room(room_id)
+    if room and is_room_admin(room_id, current_user.username):
+        existing_room_members = [member['_id']['username'] for member in get_room_members(room_id)]
+        room_members_str = ",".join(existing_room_members)
+        message = ''
+        if request.method == 'POST':
+            room_name = request.form.get('room_name')
+            room['name'] = room_name
+            update_room(room_id, room_name)
+
+            new_members = [username.strip() for username in request.form.get('members').split(',')]
+            members_to_add = list(set(new_members) - set(existing_room_members))
+            members_to_remove = list(set(existing_room_members) - set(new_members))
+            if len(members_to_add):
+                add_room_members(room_id, room_name, members_to_add, current_user.username)
+            if len(members_to_remove):
+                remove_room_members(room_id, members_to_remove)
+            message = 'Room edited successfully'
+            room_members_str = ",".join(new_members)
+        return render_template('edit_room.html', room=room, room_members_str=room_members_str, message=message)
+    else:
+        return "Room not found", 404
+
+
+@app.route('/rooms/<room_id>/messages/')
+@login_required
+def get_older_messages(room_id):
+    room = get_room(room_id)
+    if room and is_room_member(room_id, current_user.username):
+        page = int(request.args.get('page', 0))
+        messages = get_messages(room_id, page)
+        return dumps(messages)
+    else:
+        return 'Room not found', 404
 
 
 @socketio.on('join_room')
@@ -84,7 +148,11 @@ def handle_join_room_event(data):
 
 @socketio.on('send_message')
 def handle_send_message_event(data):
-    app.logger.info(f'{data["username"]} has sent message: "{data["message"]}" to the room {data["room"]}.')
+    app.logger.info("{} has sent message to the room {}: {}".format(data['username'],
+                                                                    data['room'],
+                                                                    data['message']))
+    data['created_at'] = datetime.now().strftime("%d %b, %H:%M")
+    save_message(data['room'], data['message'], data['username'])
     socketio.emit('receive_message', data, room=data['room'])
 
 
--- db.py
+++ db.py
@@ -1,15 +1,19 @@
-from pymongo import MongoClient
+from datetime import datetime
+from bson import ObjectId
+from pymongo import MongoClient, DESCENDING
 from werkzeug.security import generate_password_hash
 from users import User
 import os
 
 mongo_uri = os.environ.get('MONGO_URI')
-
-
 client = MongoClient(mongo_uri)
+MESSAGE_FETCH_LIMIT = 3
 
 db = client.get_database("ChatDB")
 users = db.get_collection("users")
+rooms = db.get_collection("rooms")
+room_members = db.get_collection("room_members")
+messages = db.get_collection("messages")
 
 
 def save_user(username, email, password):
@@ -20,3 +24,63 @@ def save_user(username, email, password):
 def get_user(username):
     user_data = users.find_one({'_id': username})
     return User(user_data['_id'], user_data['email'], user_data['password'])
+
+
+def save_room(room_name, created_by):
+    room_id = rooms.insert_one({'name': room_name, 'created_by': created_by, 'created_at': datetime.now()}).inserted_id
+    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
+    return room_id
+
+
+def update_room(room_id, room_name):
+    rooms.update_one({'_id': ObjectId(room_id)}, {'$set': {'name': room_name}})
+    room_members.update_many({'_id.room_id': ObjectId(room_id)}, {'$set': {'room_name': room_name}})
+
+
+def get_room(room_id):
+    return rooms.find_one({'_id': ObjectId(room_id)})
+
+
+def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
+    room_members.insert_one({'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name,
+                             'added_by': added_by, 'added_at': datetime.now(), 'is_room_admin': is_room_admin})
+
+
+def add_room_members(room_id, room_name, usernames, added_by):
+    room_members.insert_many([{'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name,
+                               'added_by': added_by, 'added_at': datetime.now(), 'is_room_admin': False}
+                              for username in usernames])
+
+
+def remove_room_members(room_id, usernames):
+    room_members.delete_many({'_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username}
+                                              for username in usernames]}})
+
+
+def get_room_members(room_id):
+    return list(room_members.find({'_id.room_id': ObjectId(room_id)}))
+
+
+def get_rooms_for_user(username):
+    return list(room_members.find({'_id.username': username}))
+
+
+def is_room_member(room_id, username):
+    return room_members.count_documents({'_id': {'room_id': ObjectId(room_id), 'username': username}})
+
+
+def is_room_admin(room_id, username):
+    return room_members.count_documents({'_id': {'room_id': ObjectId(room_id), 'username': username}})
+
+
+def save_message(room_id, text, sender):
+    messages.insert_one({'room_id': room_id, 'text': text, 'sender': sender, 'created_at': datetime.now()})
+
+
+def get_messages(room_id, page=0):
+    offset = page * MESSAGE_FETCH_LIMIT
+    total_messages = list(
+        messages.find({'room_id': room_id}).sort('_id', DESCENDING).limit(MESSAGE_FETCH_LIMIT).skip(offset))
+    for message in total_messages:
+        message['created_at'] = message['created_at'].strftime("%d %b, %H:%M")
+    return total_messages[::-1]
